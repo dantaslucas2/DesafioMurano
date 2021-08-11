19e2d23e8b8c7a05d8e57aa124c287d222d5f6b5
Maximo <lucas_maximo_dantas@hotmail.com>
2021-08-10 21:16:06 -0300

    5

--- 5/all.go
+++ 5/all.go
@@ -2,31 +2,59 @@ package main
 import (
 	"math/rand"
 	"fmt"
+	"sync"
 )
 
-const n int = 100000000
+const n int = 100000
 
 func main() {
 	var numerosAleatorios [n]int 
+	
+	gerarNumerosAleatorios(&numerosAleatorios)
+
+	goroutines(&numerosAleatorios,2) // vetor de números aleatórios, quantidade de threads
+}
+
+//--------------------Gera números aleatórios
+func gerarNumerosAleatorios (numerosAleatorios* [n]int){
 	min := -50
 	max := 50
-	for i := 0; i < 10; i++ {
-		numerosAleatorios[i] = (rand.Intn(max - min) + min)
+	for i := 0; i < n; i++ {
+		numerosAleatorios[i] += (rand.Intn(max - min) + min)
 		fmt.Println("int: ",numerosAleatorios[i])
 	}
-
-	goroutines(&numerosAleatorios,1)
 }
 
+//--------------------Cria threads
 func goroutines(numerosAleatorios* [n]int, k int){
 	var tamanho int = len(numerosAleatorios)/k
-	fmt.Println(tamanho)
+	var valorFinal int
+	fmt.Println("tamanho = ",tamanho)
+	var wg sync.WaitGroup
+	var mu sync.Mutex
 
 	for i := 0; i < k; i++ {
-		go soma(numerosAleatorios, tamanho, i)
+		fmt.Println("thread ",i," iniciando")
+		wg.Add(1)
+		go soma(numerosAleatorios, tamanho, i, &wg, &mu, &valorFinal)
 	}
+
+	wg.Wait()
+	fmt.Println("Valor total do vetor: ",valorFinal)
 }
 
-func soma(numerosAleatorios* [n]int, tamanho int, i int){
-	
+//--------------------Função onde as threads irão realizar a soma
+func soma(numerosAleatorios* [n]int, tamanho int, i int, wg *sync.WaitGroup, mu *sync.Mutex, valorFinal* int){ 
+	var total int  
+
+	for o := i*tamanho; o < tamanho*(i + 1); o++ { 
+		fmt.Println("thread : ", i ," contando")
+		total += numerosAleatorios[o]
+	}
+
+	fmt.Println("thread : ",i, " somou : ", total)
+	mu.Lock()
+		*valorFinal += total
+	mu.Unlock()
+	defer wg.Done()
 }
